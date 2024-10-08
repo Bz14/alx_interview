b0ce00eea301d86104978643c19174a9014e4fcb
Bz14 <bezueyerusalem@gmail.com>
2024-10-08 08:40:27 +0300

    Files added.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ A module to implement lockboxes """ 
-from collections import defaultdict
+from collections import deque
 
 
 def canUnlockAll(boxes):
     """ A function that determines if all the boxes can be opened """
+    queue = deque([0])
     visited = set([0])
-    def DFS(node):
+    while queue:
+        node = queue.popleft()
         for child in boxes[node]:
             if child not in visited:
                 visited.add(child)
-                DFS(child)
-    
-    DFS(0)
+                queue.append(child)
     return len(visited) == len(boxes)
