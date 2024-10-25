6d84efd02d48114c710ff45bf6e1461fa9630a67
Bz14 <bezueyerusalem@gmail.com>
2024-10-25 15:03:02 +0300

    Log parsing.

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,39 @@
 #!/usr/bin/python3
-""" Log parsing """
+"""
+Log parsing
+"""
+
 import sys
-import re
 
+if __name__ == '__main__':
 
-def print_stats(file_size, status_codes):
-    """ Print stats """
-    print("File size: {}".format(file_size))
-    for k, v in sorted(status_codes.items()):
-        if v:
-            print("{}: {}".format(k, v))
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-
-def parse_line(line, file_size, status_codes):
-    """ Parse line """
-    try:
-        data = line.split()
-        file_size += int(data[-1])
-        if data[-2] in status_codes:
-            status_codes[data[-2]] += 1
-        return file_size, status_codes
-    except Exception:
-        return file_size, status_codes
-
-
-if __name__ == "__main__":
-    regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')
-    file_size = 0
-    status_codes = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-    }
-    count = 0
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
     try:
         for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if match:
-                count += 1
-                status = match.group(1)
-                size = int(match.group(2))
-                file_size += size
-                if status.isDecimal():
-                    status_codes[status] += 1
-                
-                if count % 10 == 0:
-                    print_stats(file_size, status_codes)
-    finally:
-        print_stats(file_size, status_codes)
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
