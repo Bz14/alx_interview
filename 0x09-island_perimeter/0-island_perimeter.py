35c2c24eb27a024bceb8f93d4d6912b9ede4f1af
Bz14 <bezueyerusalem@gmail.com>
2024-12-05 12:54:54 +0300

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,36 +1,41 @@
 #!/usr/bin/python3
-""" Island perimeter"""
+"""Island Perimeter using BFS"""
 
 
 def island_perimeter(grid):
-    """island perimeter"""
+    """Calculate the perimeter of the island using BFS."""
     if not grid or not grid[0]:
         return 0
+
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    row_len, col_len = len(grid), len(grid[0])
     visited = set()
-    ans = 0
-    row_len = len(grid)
-    col_len = len(grid[0])
 
     def is_inbound(row, col):
-        """is inbound"""
-        return (row >= 0 and row < row_len) and (col >= 0 and col < col_len)
+        """Check if the position is within the grid bounds."""
+        return 0 <= row < row_len and 0 <= col < col_len
 
-    def DFS(row, col):
-        """DFS"""
-        nonlocal ans
-        if not is_inbound(row, col) or grid[row][col] == 0:
-            ans += 1
-            return
-        visited.add((row, col))
-        for x, y in directions:
-            new_row = row + x
-            new_col = col + y
-            if (new_row, new_col) not in visited:
-                DFS(new_row, new_col)
+    def BFS(start_row, start_col):
+        """Perform BFS to calculate the perimeter."""
+        queue = [(start_row, start_col)]
+        visited.add((start_row, start_col))
+        perimeter = 0
+
+        while queue:
+            row, col = queue.pop(0)
+            for x, y in directions:
+                new_row, new_col = row + x, col + y
+                if (not is_inbound(new_row, new_col) or grid[new_row][new_col] == 0):
+                    perimeter += 1
+                elif ((new_row, new_col) not in visited
+                      and grid[new_row][new_col] == 1):
+                    visited.add((new_row, new_col))
+                    queue.append((new_row, new_col))
+        return perimeter
 
     for i in range(row_len):
         for j in range(col_len):
             if grid[i][j] == 1:
-                DFS(i, j)
-                return ans
+                return BFS(i, j)
+
+    return 0
